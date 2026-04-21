528ecfae047c2672f5c880dcb6524a0ba0ed2b5d
Agasi Miroyan <limekick@mail.ru>
2026-04-21 20:52:25 +0300

    grafana added

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ from prometheus_client import generate_latest
 
 from app.api.v1 import routers
 from app.init import redis_manager
+from app.log import log_requests
 
 
 @asynccontextmanager
@@ -20,7 +21,7 @@ async def lifespan(_: FastAPI) -> AsyncIterator[None]:
 
 app = FastAPI(lifespan=lifespan, title="Finance Tracker")
 
-# app.middleware("http")(log_requests)
+app.middleware("http")(log_requests)
 
 
 @app.get("/metrics")
