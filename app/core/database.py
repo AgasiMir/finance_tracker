518b2f63d443fa160ca1e7b9e5d7a6ae7c583fc9
Agasi Miroyan <limekick@mail.ru>
2026-04-23 10:58:48 +0300

    engine set

--- app/core/database.py
+++ app/core/database.py
@@ -8,7 +8,10 @@ from app.config import settings
 async_engine = create_async_engine(
     url=settings.DB_URL,
     pool_pre_ping=True,
+    pool_size=10,
+    max_overflow=20,
     pool_timeout=30,
+    pool_recycle=3600,
     echo=False,
 )
 
